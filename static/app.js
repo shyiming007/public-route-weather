d21a85876f9a870dee274b9d676233ee41caff43
shyiming007 <115174505+shyiming007@users.noreply.github.com>
2023-04-02 00:07:37 +0100

    Update app.js

--- static/app.js
+++ static/app.js
@@ -1,15 +1,15 @@
-var jqxhr = $.getJSON("/stations", function(data) {
-    console.log( "success", data);
-})
-    .done(function() {
-    console.log( "second success" );
-    })
-    .fail(function() {
-    console.log( "error" );
-    })
-    .always(function() {
-    console.log( "complete" );
-    });
+// var jqxhr = $.getJSON("/stations", function(data) {
+//     console.log( "success", data);
+// })
+//     .done(function() {
+//     console.log( "second success" );
+//     })
+//     .fail(function() {
+//     console.log( "error" );
+//     })
+//     .always(function() {
+//     console.log( "complete" );
+//     });
 
 // var jqxhr = $.getJSON($SCRIPT_ROOT + "/occupancy/" + marker.station_number,
 //     function(data) {
@@ -33,4 +33,4 @@ var jqxhr = $.getJSON("/stations", function(data) {
 //     }).fail(function() {
 //     console.log( "error" );
 //     })
-    
+
