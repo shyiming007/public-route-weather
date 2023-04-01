de9fd32c2b198d705aeb8617199548680c5255ea
shyiming007 <115174505+shyiming007@users.noreply.github.com>
2023-04-01 12:51:42 +0100

    Update app.py

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ import traceback
 import sqlalchemy as sqla
 from sqlalchemy import create_engine, DateTime, text
 import functools
+import pandas as pd
 
 # Database connection
 URI = "dbbikes.ci3iggfwlke6.eu-west-1.rds.amazonaws.com"
@@ -30,26 +31,13 @@ app = Flask(__name__)
 
 @app.route("/")
 def main():
+    
     return render_template("index.html", apikey=config.APIKEY)
 
 
-# @app.route('/stations')
-# def stations():
-# # get db connection
-#     # return list of stations
-#     pass
-# @functools.lru_cache(maxsize=128)
-# def get_stations():
-#     engine = get_db()
-#     sql = "select * from station ;"
-#     try:
-#         with engine.connect() as conn:
-#             rows = conn.execute(text(sql)).fetchall()
-#             print('#found {} stations', len(rows), rows)
-#             return jsonify([row._asdict() for row in rows]) # use this formula to turn the rows into a list of dicts
-#     except:
-#         print(traceback.format_exc())
-#         return "error in get_stations", 404
+
+
+
     
 # @app.route('/contact')
 # def contact():
@@ -80,6 +68,7 @@ def station(station_id):
 @functools.lru_cache(maxsize=128)
 def get_stations():
     engine = create_engine(f"mysql+mysqlconnector://{USER}:{PASSWORD}@{URI}:{PORT}/{DB}", echo=True, connect_args={'autocommit': True})
+    
     sql = "SELECT * FROM station LIMIT 100;"
     try:
         with engine.connect() as conn:
@@ -90,8 +79,21 @@ def get_stations():
         print(traceback.format_exc())
         return "error in get_stations", 404
 
+@app.route("/occupancy/<int:station_id>")
+def get_occupancy(station_id):
 
-
+    engine = create_engine(f"mysql+mysqlconnector://{USER}:{PASSWORD}@{URI}:{PORT}/{DB}", echo=True, connect_args={'autocommit': True})
+    with engine.connect() as conn:
+        query = text("""select * from availability where number = :number""")
+        df = pd.read_sql_query(query, conn, params={"number":station_id})
+        df['last_update_date'] = pd.to_datetime(df.last_update, unit='ms')
+        df.set_index('last_update_date', inplace=True)
+        res = df['available_bike_stands'].resample('1d').mean()
+        #res['dt'] = df.index
+        print(res)
+        
+        return jsonify(data=json.dumps(list(zip(map(lambda x: x.isoformat(), res.index), res.values))))
+    
 
 if __name__ == "__main__":
     app.run(debug=True)
