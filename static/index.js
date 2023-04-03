c8fb96a5072601a1ba86aa424e07761d29d7927d
shyiming007 <115174505+shyiming007@users.noreply.github.com>
2023-04-03 21:47:10 +0100

    Update index.js

--- static/index.js
+++ static/index.js
@@ -1,3 +1,6 @@
+// referencing from the website https://www.youtube.com/watch?v=fc-D9eZmFFA
+// referencing from the website https://www.youtube.com/watch?v=bXV8uHEANBI&t=619s
+
 
 // $.ajax({
 //     url: "/occupancy/" + station_number,
@@ -44,6 +47,10 @@ function addMarkers(stations) {
         else{
             station_sta = 'CLOSE'
         }
+        var yellowIcon = {
+            url: 'http://maps.google.com/mapfiles/ms/icons/yellow-dot.png'
+                };
+        marker.setIcon(yellowIcon);
         
         google.maps.event.addListener(marker, 'click', function() {
             var contentString = '<div id="content"><h1>' + station.name + '</h1></div>' + '<div id="station_availability"><ul>' +
@@ -142,39 +149,7 @@ function getStations(){
 //     });
 // }
 
-// function drawHeatmap(me) {
-//     // console.log('toggle heatmap');
-//     console.log('clicked checkbox-1', me, me.prop('checked'));
-//     checked = me.prop('checked');
-//     if(checked) {
-//     if(heatmap == null) {
-//     var jqxhr = $.getJSON($SCRIPT_ROOT + "/heatmap",
-//     function(data) {
-//     console.log('data', data);
-//     var heatmapData = [];
-//     data.data.forEach((row) => {
-//     heatmapData.push(
-//     {location: new google.maps.LatLng(row.position_lat, row.position_lng),
-//     weight: row.available_bikes});
-//     });
-//     heatmap = new google.maps.visualization.HeatmapLayer({
-//     data: heatmapData,
-//     map: map
-//     });
-//     console.log(heatmap);
-//     heatmap.setMap(map);
-//     heatmap.set('radius', 40);
-//     // heatmap.setMap(heatmap.getMap() ? null : map);
-//     }).fail(function() {
-//     console.log('failed');
-//     });
-//     } else {
-//     heatmap.setMap(map);
-//     }
-//     } else {
-//     heatmap.setMap(null);
-//     }
-//     }
+
     
 let currentPosition;
 let selectedPlace;
@@ -238,13 +213,13 @@ function initMap() {
                 map: map,
             });
         }
-        // Create a blue icon for the marker
-        var blueIcon = {
-            url: 'http://maps.google.com/mapfiles/ms/icons/blue-dot.png'
-                };
+        // // Create a yellow icon
+        // var yellowIcon = {
+        //     url: 'http://maps.google.com/mapfiles/ms/icons/yellow-dot.png'
+        //         };
   
-        // Set the icon of the marker to the blue icon
-        marker_des.setIcon(blueIcon);
+        // // Set the icon to the yellow icon
+        // marker_des.setIcon(yellowIcon);
         marker_des.setPosition(selectedPlace.location);
         
         console.log(selectedPlace)
@@ -255,6 +230,9 @@ function initMap() {
         if (!directionsRenderer){
             directionsRenderer = new google.maps.DirectionsRenderer({
             map:map,
+            polylineOptions: {
+                strokeColor: '#800080' 
+              }
         });
         }
 
