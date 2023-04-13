4b0b34c04058acd7591e65a6bae2f15fabc90671
shyiming007 <115174505+shyiming007@users.noreply.github.com>
2023-04-13 23:08:05 +0100

    Update app.py

--- app.py
+++ app.py
@@ -37,19 +37,6 @@ def main():
 
 
 
-
-
-    
-# @app.route('/contact')
-# def contact():
-# # get db connection
-#     return app.send_static_file('contact.html')
-
-
-# @app.route('/realtime')
-# def realtime():
-#     return app.send_static_file('realtime.html')
-
 @app.route('/station/<int:station_id>')
 def station(station_id):
 # show the station with the given id, the id is an integer
@@ -106,6 +93,24 @@ def get_weather():
             return jsonify(row._asdict())
         else:
             return "No weather data available", 404
+
+@app.route('/hour')
+def get_hour():
+    engine = create_engine(f"mysql+mysqlconnector://{USER}:{PASSWORD}@{URI}:{PORT}/{DB}", echo=True, connect_args={'autocommit': True})
+    with engine.connect() as conn:
+        sql = "SELECT DATE_FORMAT(timestamp, '%Y-%m-%d %H:00:00') AS hour, AVG(available_bikes) AS average_bikes_hour, AVG(available_bike_stands) AS average_bike_stands_hour, number FROM dbbikes.completedata GROUP BY number,hour"
+        try:
+            with engine.connect() as conn:
+                rows = conn.execute(text(sql)).fetchall()
+                print('#found {} stations', len(rows), rows)
+                return jsonify([row._asdict() for row in rows]) # use this formula to turn the rows into a list of dicts
+        except:
+            print(traceback.format_exc())
+            return "error in get_stations", 404
+
+
+
+
 @app.route('/showPred/<int:predNum>')
 def showPred(predNum):
     showString = 'The predicted number of the available bikes is '
