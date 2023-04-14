20e999f49612a5581f515a54baf9f3fd31c7c3bb
shyiming007 <115174505+shyiming007@users.noreply.github.com>
2023-04-14 13:21:47 +0100

    Update index.js

--- static/index.js
+++ static/index.js
@@ -2,28 +2,7 @@
 // referencing from the website https://www.youtube.com/watch?v=bXV8uHEANBI&t=619s
 
 
-// $.ajax({
-//     url: "/occupancy/" + station_number,
-//     success: function(response){
-//       var data = JSON.parse(response.data);
-      
-//     }
-//   });
-// fetch('/occupancy/12')
-//         .then(response => response.json())
-//         .then(data => {
-//         const firstValue = data.data[0][1];
-//         console.log(`The first value of res is: ${firstValue}`);
-//         });
-// function getOccupancy(stationId, callback) {
-//     fetch(`/occupancy/${stationId}`)
-//     .then((response) => response.json())
-//     .then((data) => {
-//         console.log("fetch response", typeof data);
-//         static occupancy = JSON.parse(data.data)[2][3];
-//         callback(occupancy);
-//     });
-// }
+
     var btn1 = document.querySelector('.btn1');
     var btn2 = document.querySelector('.btn2');
     var realTimeDiv = document.querySelector('.search_real_time');
@@ -95,41 +74,18 @@ function addMarkers(stations) {
 
 
 
-            // // var jqxhr = $.getJSON($SCRIPT_ROOT + "/occupancy/" + marker.station_number, function(data) {
-            //     data = JSON.parse(data.data);
-            //     console.log('data', data);
-            
-            //     var node = document.createElement('div'),
-            //     // infowindow = new google.maps.InfoWindow(),
-            //     chart = new google.visualization.ColumnChart(node);
-            
-            //     var chart_data = new google.visualization.DataTable();
-            //     chart_data.addColumn('datetime', 'Time of Day');
-            //     chart_data.addColumn('number', '#');
-            //     _.forEach(data, function(row){
-            //     chart_data.addRow([new Date(row[0]), row[1]]);
-            //     })
-            
-            //     chart.draw(chart_data, options);
-                // infowindow.setContent(node);
-                // infowindow.open(marker.getMap(), marker);
-                // }).fail(function() {
-                // console.log( "error" );
-                // })
-            // var currentInfoWindow;
+
 
             var infowindow = new google.maps.InfoWindow({
                 content: contentString
             
             });
-            // station_choose = station.number;
+
             
             getHour(station.number)
-            // if (currentInfoWindow) {
-            //     currentInfoWindow.close();
-            //         }
+
             infowindow.open(map, marker);
-            // currentInfoWindow = infowindow;
+
             
         });
     });
@@ -152,14 +108,8 @@ function getHour(n){
     fetch("/hour")
     .then((response) => response.json())
     .then((data) => {
-        console.log("fetch response", data);
-        // const filteredData = data.find(item => item.number === 44);
-        // if(filteredData){
-        //     $("#average").text(`The hourly average bikes is ${filteredData.average_bikes_hour}`);
-        // }
-        // else{
-        //     console.log("No data found with number equal to 44");
-        // }
+        console.log("fetch response", data['hour'][0]);
+ 
         displayChart(data, n, '2023-04-09');
         displayChart2(data, n, '2023-04-09');
 
@@ -266,15 +216,7 @@ function displayChart2(data, stationNumber, day) {
       }
     bikeChart2 = new Chart('bike-chart2', chartConfig2);
   }
-// function getOccupancy(){
-//     fetch("/occupancy/{station_id}")
-//     .then((response) => response.json())
-//     .then((data) => {
-//         console.log("fetch response", typeof data);
-//         addOccupancy(data);
-    
-//     });
-// }
+
 
 
     
@@ -293,116 +235,212 @@ function initMap() {
     zoom: 14,
     center: dublin,
     });
-    // The marker, positioned at Dublin
-    // const marker = new google.maps.Marker({
-    // position: dublin,
-    // map: map,
-    // });
-    // getOccupancy();
+
     getStations();
-    // getHour();
+
     
-    navigator.geolocation.getCurrentPosition(function(position){
-        currentPosition = {
-            lat: position.coords.latitude,
-            lng: position.coords.longitude,
-        }
+//     navigator.geolocation.getCurrentPosition(function(position){
+//         currentPosition = {
+//             lat: position.coords.latitude,
+//             lng: position.coords.longitude,
+//         }
         
-        map.setCenter(currentPosition);
-        map.setZoom(13);
-        const autocomplete = new google.maps.places.Autocomplete(
-            document.getElementById('search_des'),
-            {
+//         map.setCenter(currentPosition);
+//         map.setZoom(13);
+//         const autocomplete = new google.maps.places.Autocomplete(
+//             document.getElementById('search_des'),
+//             {
             
-            bounds:{
-                east: currentPosition.lng + 0.001,
-                west: currentPosition.lng - 0.001,
-                south: currentPosition.lat - 0.001,
-                north: currentPosition.lat + 0.001,
+//             bounds:{
+//                 east: currentPosition.lng + 0.001,
+//                 west: currentPosition.lng - 0.001,
+//                 south: currentPosition.lat - 0.001,
+//                 north: currentPosition.lat + 0.001,
             
+//             },
+//             strictBounds: false,
+
+//             }
+//         )
+//         autocomplete.addListener('place_changed', function(){
+//         const place = autocomplete.getPlace();
+        
+//         selectedPlace = {
+//             location: place.geometry.location,
+//             name: place.name,
+//             address: place.formatted_address,
+//             placeId: place.place_id,
+//         };
+        
+//         map.setCenter(selectedPlace.location);
+
+//         if (!marker_des){
+//             marker_des = new google.maps.Marker({
+//                 map: map,
+//             });
+//         }
+//         // // Create a yellow icon
+//         // var yellowIcon = {
+//         //     url: 'http://maps.google.com/mapfiles/ms/icons/yellow-dot.png'
+//         //         };
+  
+//         // // Set the icon to the yellow icon
+//         // marker_des.setIcon(yellowIcon);
+//         marker_des.setPosition(selectedPlace.location);
+        
+        
+//         if (!directionsService){
+//             directionsService = new google.maps.DirectionsService();
+//         }
+
+//         if (!directionsRenderer){
+//             directionsRenderer = new google.maps.DirectionsRenderer({
+//             map:map,
+//             polylineOptions: {
+//                 strokeColor: '#800080' 
+//               }
+//         });
+//         }
+
+//         directionsRenderer.set('directions',null)
+    
+//         directionsService.route({
+//             origin:new google.maps.LatLng(
+//                 currentPosition.lat,
+//                 currentPosition.lng
+//             ),
+//             destination:{
+//                 placeId: selectedPlace.placeId,
+//             },
+//             travelMode: 'BICYCLING',
+//         },
+//         function(response, status){
+//             if (status == 'OK'){
+//                 directionsRenderer.setDirections(response);
+
+//                 if(!info_des){
+//                     info_des = new google.maps.InfoWindow();
+
+//                 }
+//                 info_des.setContent(
+//                     `
+//                     <h3>${selectedPlace.name}</h3>
+//                     <div>Address: ${selectedPlace.address}</div>
+//                     <div>Location: ${selectedPlace.location}</div>
+//                     <div>Ride time: ${response.routes[0].legs[0].duration.text}</div>
+//                     `
+//                 );
+//                 info_des.open(map,marker_des)
+//             }
+//         }
+//         );
+
+//     });
+
+// });
+// build a real-time route
+navigator.geolocation.getCurrentPosition(function(position) {
+    map.setCenter(dublin);
+    map.setZoom(13);
+    const autocomplete_start = new google.maps.places.Autocomplete(
+        document.getElementById('search_start_real'), {
+            bounds: {
+                east: dublin.lng + 0.001,
+                west: dublin.lng - 0.001,
+                south: dublin.lat - 0.001,
+                north: dublin.lat + 0.001,
             },
             strictBounds: false,
-
-            }
-        )
-        autocomplete.addListener('place_changed', function(){
-        const place = autocomplete.getPlace();
-        
-        selectedPlace = {
-            location: place.geometry.location,
-            name: place.name,
-            address: place.formatted_address,
-            placeId: place.place_id,
-        };
-        
-        map.setCenter(selectedPlace.location);
-
-        if (!marker_des){
-            marker_des = new google.maps.Marker({
-                map: map,
-            });
         }
-        // // Create a yellow icon
-        // var yellowIcon = {
-        //     url: 'http://maps.google.com/mapfiles/ms/icons/yellow-dot.png'
-        //         };
-  
-        // // Set the icon to the yellow icon
-        // marker_des.setIcon(yellowIcon);
-        marker_des.setPosition(selectedPlace.location);
+    );
+    autocomplete_start.addListener('place_changed', function() {
+        place_start = autocomplete_start.getPlace();
         
-        
-        if (!directionsService){
-            directionsService = new google.maps.DirectionsService();
-        }
-
-        if (!directionsRenderer){
-            directionsRenderer = new google.maps.DirectionsRenderer({
-            map:map,
-            polylineOptions: {
-                strokeColor: '#800080' 
-              }
-        });
-        }
-
-        directionsRenderer.set('directions',null)
-    
-        directionsService.route({
-            origin:new google.maps.LatLng(
-                currentPosition.lat,
-                currentPosition.lng
-            ),
-            destination:{
-                placeId: selectedPlace.placeId,
-            },
-            travelMode: 'BICYCLING',
-        },
-        function(response, status){
-            if (status == 'OK'){
-                directionsRenderer.setDirections(response);
-
-                if(!info_des){
-                    info_des = new google.maps.InfoWindow();
-
-                }
-                info_des.setContent(
-                    `
-                    <h3>${selectedPlace.name}</h3>
-                    <div>Address: ${selectedPlace.address}</div>
-                    <div>Location: ${selectedPlace.location}</div>
-                    <div>Ride time: ${response.routes[0].legs[0].duration.text}</div>
-                    `
-                );
-                info_des.open(map,marker_des)
-            }
-        }
-        );
-
-    });
-
 });
 
+    const autocomplete_des = new google.maps.places.Autocomplete(
+        document.getElementById('search_des_real'), {
+            bounds: {
+                east: dublin.lng + 0.001,
+                west: dublin.lng - 0.001,
+                south: dublin.lat - 0.001,
+                north: dublin.lat + 0.001,
+            },
+            strictBounds: false,
+        }
+        
+    );
 
+
+        autocomplete_des.addListener('place_changed', function() {
+            const place_des = autocomplete_des.getPlace();
+
+            selectedPlace = {
+                location: place_des.geometry.location,
+                name: place_des.name,
+                address: place_des.formatted_address,
+                placeId: place_des.place_id,
+            };
+
+            map.setCenter(selectedPlace.location);
+
+            if (!marker_des) {
+                marker_des = new google.maps.Marker({
+                    map: map,
+                });
+            }
+
+            marker_des.setPosition(selectedPlace.location);
+
+            if (!directionsService) {
+                directionsService = new google.maps.DirectionsService();
+            }
+
+            if (!directionsRenderer) {
+                directionsRenderer = new google.maps.DirectionsRenderer({
+                    map: map,
+                    polylineOptions: {
+                        strokeColor: '#800080'
+                    }
+                });
+            }
+
+            directionsRenderer.set('directions', null)
+
+            directionsService.route({
+                    origin: new google.maps.LatLng(
+                        place_start.geometry.location.lat(),
+                        place_start.geometry.location.lng()
+                    ),
+                    destination: {
+                        placeId: selectedPlace.placeId,
+                    },
+                    travelMode: 'BICYCLING',
+                },
+                function(response, status) {
+                    if (status == 'OK') {
+                        directionsRenderer.setDirections(response);
+
+                        if (!info_des) {
+                            info_des = new google.maps.InfoWindow();
+
+                        }
+                        info_des.setContent(
+                            `
+                            <h3>${selectedPlace.name}</h3>
+                            <div>Address: ${selectedPlace.address}</div>
+                            <div>Location: ${selectedPlace.location}</div>
+                            <div>Ride time: ${response.routes[0].legs[0].duration.text}</div>
+                            `
+                        );
+                        info_des.open(map, marker_des)
+                    }
+                }
+            );
+        });
+    
+});
+//build a route of predicition
 navigator.geolocation.getCurrentPosition(function(position) {
     map.setCenter(dublin);
     map.setZoom(13);
@@ -515,12 +553,3 @@ navigator.geolocation.getCurrentPosition(function(position) {
 var map = null;
 window.initMap = initMap;
 
-
-
-
-
-// $(document).ready(function() {
-//     $.get('/hour', function(data) {
-//       $('#result').text('The hourly average bikes is ' + data);
-//     });
-//   });
